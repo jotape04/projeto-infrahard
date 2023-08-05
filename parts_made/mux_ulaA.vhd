026635be470db5d74b68bf1089dbe7e9de42fa21
Rafael Rios <108680246+RafaelRaios@users.noreply.github.com>
2023-08-04 21:44:59 -0300

    .

--- parts_made/mux_ulaA.vhd
+++ parts_made/mux_ulaA.vhd
@@ -1,4 +1,11 @@
 module mux_ulaA(
+    input wire selector,
     input wire [31:0] PC,
-    output wire [31:0] Data_out
-)
+    input wire [31:0] Reg_A,
+    input wire [31:0] MDR,
+    output wire [31:0] Scr_A
+);
+
+    assign Scr_A = {} 
+
+endmodule
