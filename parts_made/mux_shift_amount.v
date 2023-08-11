b2cd0af35b13d7f502d5a655215ecddd8f369518
joaohl19 <108703483+joaohl19@users.noreply.github.com>
2023-08-10 22:44:27 -0300

    Add files via upload

---
+++ parts_made/mux_shift_amount.v
@@ -0,0 +1,19 @@
+module mux_shift_amount(
+    input wire [31:0]Reg_B,
+    input wire [4:0]instruction_10_6
+    input wire ShiftAmt,
+
+    output reg [4:0]out
+);
+
+always@(*) begin
+    case (ShiftAmt)
+        
+        1'b0: out = Reg_B[4:0];
+        1'b1: out = instruction_10_6;
+
+    endcase
+
+  end
+
+endmodule
