b4c18207f85eb0ba759eb4a5e887cefe61c4f517
joaohl19 <108703483+joaohl19@users.noreply.github.com>
2023-08-10 22:11:01 -0300

    Update mux_branch_ctrl.v

--- parts_made/mux_branch_ctrl.v
+++ parts_made/mux_branch_ctrl.v
@@ -1,8 +1,6 @@
 module mux_branch_ctrl(
     input wire [1:0]branch_ctrl,
     input wire gt,
-    input wire  not_gt,
-    input wire not_eq,
     input wire eq,
 
     input wire PCWriteCond,
@@ -15,8 +13,8 @@ always@(*) begin
     case (branch_ctrl)
         
         2'b00: out = (gt & PCWriteCond) | PCWrite ;
-        2'b01: out = (not_gt & PCWriteCond) | PCWrite;
-        2'b10: out = (not_eq & PCWriteCond) | PCWrite;
+        2'b01: out = (~gt & PCWriteCond) | PCWrite;
+        2'b10: out = (~eq & PCWriteCond) | PCWrite;
         2'b11: out = (eq & PCWriteCond) | PCWrite ;
 
     endcase
