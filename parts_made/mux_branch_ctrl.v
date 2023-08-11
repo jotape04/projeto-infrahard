6241f8d64fefaa431cae4dc0c13e588fff826986
joaohl19 <108703483+joaohl19@users.noreply.github.com>
2023-08-10 22:08:01 -0300

    Add files via upload

---
+++ parts_made/mux_branch_ctrl.v
@@ -0,0 +1,26 @@
+module mux_branch_ctrl(
+    input wire [1:0]branch_ctrl,
+    input wire gt,
+    input wire  not_gt,
+    input wire not_eq,
+    input wire eq,
+
+    input wire PCWriteCond,
+    input wire PCWrite,
+
+    output reg out
+);
+
+always@(*) begin
+    case (branch_ctrl)
+        
+        2'b00: out = (gt & PCWriteCond) | PCWrite ;
+        2'b01: out = (not_gt & PCWriteCond) | PCWrite;
+        2'b10: out = (not_eq & PCWriteCond) | PCWrite;
+        2'b11: out = (eq & PCWriteCond) | PCWrite ;
+
+    endcase
+
+  end
+
+endmodule
