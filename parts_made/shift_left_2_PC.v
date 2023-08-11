272e7cbd5c036aa3c9742727b5ce925e0f23a07c
rodrigoladvocat <rrlc@cin.ufpe.br>
2023-08-11 02:38:43 -0300

    shift_left_2_pc corrigido com a entrada do PC

--- parts_made/shift_left_2_PC.v
+++ parts_made/shift_left_2_PC.v
@@ -1,14 +1,19 @@
 module shift_left_2_PC (
+  input wire [31:0] pc, 
   input wire [4:0] rs,
   input wire [4:0] rt,
   input wire [15:0] offset, //veio de {Instr25_21, Instr20_16, Instr15_0} do Instr_Reg
-  output wire [27:0] data_out //vai para jump_address_31_0 do mux_pcsource
+  output wire [31:0] data_out //vai para jump_address_31_0 do mux_pcsource
 );
+
+  wire [3:0] pc_4most;
+
+  assign pc_4most = pc[31:28];
 
   wire [25:0] data_in;
 
   assign data_in = {rs, rt, offset}
 
-  assign data_out = {data_in, {2{1'b0}}}; //concatena data_in (MSB) com o número 0 extendido para 2 bits (LSB)
+  assign data_out = {pc_4most, data_in, {2{1'b0}}}; //concatena data_in (MSB) com o número 0 extendido para 2 bits (LSB)
 
 endmodule
