dee30cbe1d4819baaf333ccde78289c79611c006
rodrigoladvocat <rrlc@cin.ufpe.br>
2023-08-11 02:27:33 -0300

    shift_left_2_pc

--- parts_made/shift_left_2_PC.v
+++ parts_made/shift_left_2_PC.v
@@ -1,7 +1,13 @@
 module shift_left_2_PC (
-  input wire [25:0] data_in, //veio de {Instr25_21, Instr20_16, Instr15_0} do Instr_Reg
+  input wire [4:0] rs,
+  input wire [4:0] rt,
+  input wire [15:0] offset, //veio de {Instr25_21, Instr20_16, Instr15_0} do Instr_Reg
   output wire [27:0] data_out //vai para jump_address_31_0 do mux_pcsource
 );
+
+  wire [25:0] data_in;
+
+  assign data_in = {rs, rt, offset}
 
   assign data_out = {data_in, {2{1'b0}}}; //concatena data_in (MSB) com o número 0 extendido para 2 bits (LSB)
 
