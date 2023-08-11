e693ccd55d4e91169729402eeeb7ec55c58015b5
rodrigoladvocat <rrlc@cin.ufpe.br>
2023-08-11 00:16:05 -0300

    div e mult nao verificados

--- parts_made/mult.v
+++ parts_made/mult.v
@@ -1,38 +1,38 @@
 module mult(
     input wire clk,
-    input wire MultCtrl,
+    input wire mult_ctrl,
     input wire reset,
     input wire [31:0] a,
     input wire [31:0] b,
-    output reg [31:0] high,
-    output reg [31:0] low,
+    output reg [31:0] Hi,
+    output reg [31:0] Lo,
     output reg mult_end
 
 );
 
-    integer cont = -1;
+    integer count_cycles = -1;
     reg [64:0] add;
     reg [64:0] sub;
     reg [64:0] produto;
-    reg [31:0] comp;
+    reg [31:0] complemento2;
 
     always @(posedge clk) begin
         if(reset == 1'b1) begin
-            high = 32'b0;
-            low = 32'b0;
+            Hi = 32'b0;
+            Lo = 32'b0;
             mult_end = 1'b0;
             add = 65'b0;
             sub = 65'b0;
             produto = 65'b0;
-            comp = 32'b0;
-            cont = -1;
+            complemento2 = 32'b0;
+            count_cycles = -1;
         end
-        if(MultCtrl == 1'b1) begin
+        if(mult_ctrl == 1'b1) begin
             add = {a, 33'b0};
-            comp = (~a + 1'b1);         // invert bits + add one
-            sub = {comp, 33'b0}; 
+            complemento2 = (~a + 1'b1);
+            sub = {complemento2, 33'b0}; 
             produto = {32'b0, b, 1'b0};
-            cont = 32;
+            count_cycles = 32;
             mult_end = 1'b0;
         end
 
@@ -43,21 +43,21 @@ module mult(
 
         produto = produto >>> 1;
         
-        if(cont > 0) begin
-            cont = (cont - 1);
+        if(count_cycles > 0) begin
+            count_cycles = (count_cycles - 1);
         end
 
-        if(cont == 0) begin
-            high = produto[64:33];
-            low = produto[32:1];
+        if(count_cycles == 0) begin
+            Hi = produto[64:33];
+            Lo = produto[32:1];
             mult_end = 1'b1;
 
             // reseting
             add = 65'b0;
             sub = 65'b0;
             produto = 65'b0;
-            cont = -1;
+            count_cycles = -1;
         end
     end
-
+    
 endmodule
