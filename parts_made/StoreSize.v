86a219f2cdff58b77a91ea9546719ec7511a4ce5
rodrigoladvocat <rrlc@cin.ufpe.br>
2023-08-12 17:23:12 -0300

    .

--- parts_made/StoreSize.v
+++ parts_made/StoreSize.v
@@ -9,7 +9,7 @@ module StoreSize(
 
     always @(*) begin
         case(StoreSize_Ctrl)
-            2'b00: write_data = data_in; // sw
+            2'b00: write_data = B; // sw
             2'b01: write_data = {data_in[31:16], B[15:0]}; // sh
             2'b10: write_data = {data_in[31:8], B[7:0]}; // sb
             default: write_data = 32'd0; // Valor padrão para casos não especificados
